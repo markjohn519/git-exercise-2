c0fb0dd3b8209812b177873b49716e7658b898ad
Mark John Paul M. Monton <34324883+markjohn519@users.noreply.github.com>
2022-10-05 09:46:34 +0800

    remove white spaces

--- lib.js
+++ lib.js
@@ -1,11 +1,8 @@
 const square = (n) => n * n;
 
-
 const fahrenheit = (celsius) => (celsius * 1.8) + 32;
 
-
 const rectangleArea = (a, b) => a * b;
-
 
 const isPalindrome = (str) => {
   const str2 = str.toLowerCase();
