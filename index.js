1d06407c4fda755a961e9334b9783b3b8c83bcc8
markjohn519 <markjohnmonton@gmail.com>
2022-10-05 10:10:39 +0800

    added modules in lib.js and created index.js

---
+++ index.js
@@ -0,0 +1,6 @@
+const { square, fahrenheit, rectangleArea, isPalindrome } = require('./lib');
+
+console.log(square(5));
+console.log(fahrenheit(55));
+console.log(rectangleArea(5, 7));
+console.log(isPalindrome('RaCecAr'));
