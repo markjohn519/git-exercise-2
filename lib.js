ac2412a2e9131e21ba60bc080249a3baadfebcb6
markjohn519 <markjohnmonton@gmail.com>
2022-10-11 09:14:16 +0800

    modified the index using prettier

--- lib.js
+++ lib.js
@@ -1,19 +1,19 @@
 const square = (n) => n * n;
 
-const fahrenheit = (celsius) => (celsius * 1.8) + 32;
+const fahrenheit = (celsius) => celsius * 1.8 + 32;
 
 const rectangleArea = (a, b) => a * b;
 
 const isPalindrome = (str) => {
   const str2 = str.toLowerCase();
-  const reverse =  str2.split('').reverse().join('');
-  
-  return str2 === reverse; 
-}
+  const reverse = str2.split('').reverse().join('');
+
+  return str2 === reverse;
+};
 
 module.exports = {
   square,
   fahrenheit,
   rectangleArea,
-  isPalindrome
-}
+  isPalindrome,
+};
